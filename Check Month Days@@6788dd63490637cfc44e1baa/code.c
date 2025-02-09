5874e7ef02811d8ee494e91758ea7a5ad88f0fa9
Ranjot Kaur Mondair <ranjot0677.becse24@chitkara.edu.in>
2025-02-09 15:30:40 +0530

    Solution to Check Month Days

---
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num is between 1 && 12){
+        print("the number of days in a month");
+    }
+    else{
+        print("Invalid month");
+    }
+    return 0;
+}
