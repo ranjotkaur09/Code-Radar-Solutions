e87ac16edb946be48ab26290af8782b8ec587c7a
Ranjot Kaur Mondair <ranjot0677.becse24@chitkara.edu.in>
2025-02-09 21:10:13 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    char grade;
+    scanf("%c",&grade);
+
+    if(grade == 'A'){
+        printf("Excellent");
+    }
+    else if( grade == 'B'){
+        printf("Good");
+    }
+    else if( grade == 'C'){
+        printf("Average");
+    }
+    else if( garde == 'D'){
+        printf("Below Average");
+    }
+    else{
+        printf("Fail");
+    }
+    return 0;
+}
